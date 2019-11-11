6367af4adfb582cddb034d34e44109007fd23e3a
Kenneth Buchanan <kbuchanan@Kenneths-MBP-2.fios-router.home>
2019-11-11 00:28:07 -0500

    first

---
+++ spotify_script.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+import os
+import yaml
+import json
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+def read_yaml_file(filepath):
+    """ Read YAML file """
+
+    with open(filepath, 'r') as stream:
+        data = yaml.safe_load(stream)
+
+    return data
+
+
+def get_tracks_from_playlist(track_results):
+    tracks = set()
+
+    for i, item in enumerate(track_results['items']):
+        track = item['track']
+        tracks.add(track['name'])
+
+    return list(tracks)
+
+
+def create_json_file(data):
+    with open(os.path.join(
+            'app',
+            'static',
+            'data',
+            'output.json'), 'w+') as f:
+        f.write(json.dumps(data, indent=4))
+
+    return True
+
+
+def main():
+    config = read_yaml_file('credentials.yaml')
+    USERNAME = config['username']
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=config['client_id'],
+        client_secret=config['client_secret']
+    )
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    playlists = sp.user_playlists(USERNAME)
+    graph = {'nodes': [], 'links': []}
+    all_tracks = []
+
+    while True:
+        for playlist in playlists['items'][0:2]:
+            graph['nodes'].append({'id': playlist['name'], 'nodeType': 'playlist'})
+            track_results = sp.user_playlist(
+                USERNAME, playlist['id'],
+                fields="tracks,next")['tracks']
+            tracks = get_tracks_from_playlist(track_results)
+            graph['links'].extend([{'source': track_name, 'target': playlist['name']} for track_name in tracks])
+            all_tracks.extend(tracks)
+            while track_results['next']:
+                track_results = sp.next(track_results)
+                tracks = get_tracks_from_playlist(track_results)
+                graph['links'].extend([{'source': track_name, 'target': playlist['name']} for track_name in tracks])
+                all_tracks.extend(tracks)
+
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            break
+
+    graph['nodes'].extend({'id': track, 'nodeType': 'track', 'playlists': []} for track in list(set(all_tracks)))
+
+    for link in graph['links']:
+        for node in graph['nodes']:
+            if node['nodeType'] == 'track':
+                if link['source'] == node['id']:
+                    node['playlists'].append(link['target'])
+
+    _ = create_json_file(graph)
+
+
+if __name__ == '__main__':
+    main()
---
+++ webserver.py
@@ -0,0 +1,41 @@
+import os
+import app.config as config
+from flask import Flask, render_template, \
+    send_from_directory
+
+
+app = Flask(
+    __name__,
+    static_folder=os.path.join(config.BASEDIR, "static"),
+    template_folder=os.path.join(config.BASEDIR, "templates"),
+    static_url_path=""
+)
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<path:path>')
+def send_static_data_files(path):
+    return send_from_directory('data', path)
+
+
+@app.route('/network-graph')
+def network_graph():
+    return render_template('network-graph.html')
+
+
+if __name__ == '__main__':
+
+    mode = 'development'
+
+    if mode == 'development':
+        app.config.from_object(config.Development)
+        app.run()
+    elif mode == 'production':
+        app.config.from_object(config.Production)
+        app.run()
